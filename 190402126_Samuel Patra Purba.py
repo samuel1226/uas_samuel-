e51a26fb647ae2f5b85cb7f9f7c19b1d2248a4c7
samuel1226 <85780207+samuel1226@users.noreply.github.com>
2021-06-12 13:28:14 +0700

    Add files via upload

---
+++ 190402126_Samuel Patra Purba.py
@@ -0,0 +1,46 @@
+#UAS PEMROGRAMAN
+#Samuel Patra Purba
+#Nim 190402126
+
+#1
+x = 3
+z = "Paulus"
+
+print (x,z)
+
+#2
+r = 55
+s = "Eduardo"
+t = ""
+print(type(r))
+print(type(s))
+
+#3
+b = "Eduardo is "
+c = "handsome"
+z = b + c
+print(z)
+
+#4
+string = "Dia lagi bermain bola"
+print (string)
+print (string[::-1])
+string2 = "Dia lagi bermain sepeda"
+print (string2)
+print (string2[::-2])
+
+#5
+a = "Selamat Datang di Link Saya"
+print(a)
+
+#6
+a = "Samuel Patra Purba"
+b = "190402126"
+z = a + b
+print(z)
+
+#7
+list1 = "Honda"
+list2 = "Toyota"
+print(list1)
+print(list2)
